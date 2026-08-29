8407d44eab148dacbc83a3cb07e3597db2571346
Harsh Saharan <harshsaharan.design@gmail.com>
2026-08-29 19:20:36 +0530

    feat: build workflow data model

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
-console.log("FlowForge initialized");
+const apiRequestNode = {
+    id: "node-1",
+    type: "apirequest",
+    position: {
+        x: 200,
+        y: 400
+    },
+    configuration: {
+        method: "GET",
+        url: "https://example.com"
+    }
+
+};
+const triggerNode = {
+    id: "node-2",
+    type: "trigger",
+    position: {
+        x: 100,
+        y: 300
+    }
+}
+const conditionNode = {
+    id: "node-3",
+    type: "condition",
+    position: {
+        x: 300,
+        y: 300
+    },
+    configuration: {
+        field: "followers",
+        operator: ">",
+        value: 100
+    }
+
+}
+const connection1 = {
+    source : triggerNode.id , 
+    target : apiRequestNode.id 
+
+}
+const connection2 = {
+    source : apiRequestNode.id , 
+    target : conditionNode.id
+}
+const nodes = [];
+const connections = [];
+nodes.push(apiRequestNode , triggerNode , conditionNode);
+connections.push(connection1 , connection2);
+
+
+const workflow = {nodes , connections};
